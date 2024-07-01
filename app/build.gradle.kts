71b9fd35b3f234d85ae8c866c477d7cd7d6e0a1d
Jorge Enrique Torres Mora <jorgetorresmora99@gmail.com>
2024-06-30 22:31:51 -0500

    Avance de Jeiler. Actividades diseñadas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -30,7 +31,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-}
+
+    viewBinding.enable = true
+
+}//fin bloque android
 
 dependencies {
 
@@ -39,6 +43,23 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    //PokeAPI
+    //implementation("me.sargunvohra.lib:pokekotlin:<VERSION>")
+    //library útiles
+    implementation("com.android.support:cardview-v7:28.0.0")
+    implementation("com.android.support:recyclerview-v7:28.0.0")
+
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.8.6")
+
+    //Persistencia
+    val roomversion = "2.5.0"
+    implementation("androidx.room:room-runtime:$roomversion")
+    annotationProcessor("androidx.room:room-compiler:$roomversion")
+    kapt("androidx.room:room-compiler:$roomversion")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
